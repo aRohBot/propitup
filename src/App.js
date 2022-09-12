f072ce7ef41bbb34860c2d47d822e0da44c4ca67
Alex Roh <imajndao@gmail.com>
2022-09-11 17:58:55 -0700

    puttingittogether

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <PersonCard lastName={"Doe"} firstName={"Jane"} age={"45"} hairColor={"Black"}/>
-      <PersonCard lastName={"Smith"} firstName={"John"} age={"88"} hairColor={"Brown"}/>
-      <PersonCard lastName={"Fillmore"} firstName={"Millard"} age={"50"} hairColor={"Brown"}/>
-      <PersonCard lastName={"Roh"} firstName={"Alex"} age={"33"} hairColor={"Black"}/>
+      <PersonCard lastName={"Doe"} firstName={"Jane"} age={45} hairColor={"Black"}/>
+      <PersonCard lastName={"Smith"} firstName={"John"} age={88} hairColor={"Brown"}/>
+      <PersonCard lastName={"Fillmore"} firstName={"Millard"} age={50} hairColor={"Brown"}/>
+      <PersonCard lastName={"Roh"} firstName={"Alex"} age={33} hairColor={"Black"}/>
     </div>
   );
 }
--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,13 +1,15 @@
-
+import { useState } from 'react';
 const PersonCard = (props) => {
 
     const {firstName, lastName, hairColor, age} = props;
+    const [state, setState] = useState(age)
 
     return (
         <div className="container">
             <h1>{lastName}, {firstName}</h1>
-            <p>Age: {age}</p>
+            <p>Age: {state}</p>
             <p>Hair Color: {hairColor}</p>
+            <button onClick={(e)=>setState(state+1)}>Birthday Button for {firstName} {lastName}</button>
         </div>
     );
 }
